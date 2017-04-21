650701d37beb29f16f129bceb060588096e02493
Nathalie Oostvogels <noostvog@vub.ac.be>
2017-04-21 16:29:00 +0200

    added tests for predicates

---
+++ tests/cases/compiler/interfacedeclWithPredicates1.ts
@@ -0,0 +1,8 @@
+interface UsersShow{
+    user_id?: number;
+    screen_name?: string;
+    text: string;
+} with {
+    or(and(present(user_id), not(present(screen_name))),
+       and(present(screen_name), not(present(user_id))));
+}
