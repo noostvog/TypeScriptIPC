cb9a8b629006ae090df91a8833c038d66f3edffe
Nathalie Oostvogels <noostvog@vub.ac.be>
2017-04-21 16:29:00 +0200

    added tests for predicates

---
+++ tests/cases/compiler/interfacedeclWithPredicatesObjectError3.ts
@@ -0,0 +1,14 @@
+interface UsersShow{
+    user_id?: any;
+    screen_name?: any;
+    text: any;
+} with {
+    or(and(present(user_id), not(present(screen_name))),
+    and(present(screen_name), not(present(user_id))));
+    type(user_id) == number;
+    type(screen_name) == string;
+    type(text) == string;
+    present(text);
+}
+
+let object: UsersShow = {user_id: 25, screen_name: "screen name", text: "text"}
